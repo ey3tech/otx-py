d77a319004efa59ce5cd543f9cae39a3c4196491
thrzl <thrizzle@proton.me>
2026-07-01 10:36:13 -0400

    fix: base url

--- src/otx_py/client.py
+++ src/otx_py/client.py
@@ -11,8 +11,8 @@ class OTXClient:
     __slots__ = ("api_key", "client")
     def __init__(self, api_key: str):
         self.api_key = api_key
-        self.client = Client(base_url="https://otx.alienvault.com/", headers={"X-OTX-API-KEY": api_key})
+        self.client = Client(base_url="https://otx.alienvault.com/api/v1", headers={"X-OTX-API-KEY": api_key})
 
     def pulses(self, limit: int = 25) -> PulsesResponse:
-        response = self.client.get("/otxapi/pulses", params={"limit": limit, "page": 1, "sort": "-modified"}, timeout=60)
+        response = self.client.get("https://otx.alienvault.com/otxapi/pulses", params={"limit": limit, "page": 1, "sort": "-modified"}, timeout=60)
         return decode(response.text, type=PulsesResponse)
